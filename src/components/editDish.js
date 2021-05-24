479f8fbfb4da31e9138a1654a631d7314b328347
Ishita Gupta <53005952+IshitaGupta27@users.noreply.github.com>
2021-05-24 22:16:44 +0530

    cssChanges

--- src/components/editDish.js
+++ src/components/editDish.js
@@ -5,14 +5,31 @@ import 'react-toastify/dist/ReactToastify.css';
 import AddDish from "./addDish"
 
 const EditDish = (props) => {
-    return (
-        <div className="container mt-4">
-            <button className="btn btn-dark">
-                <Link to="/admin" style={{color:'white', textDecoration:'none'}}>Back</Link></button>
-            <AddDish dish={props.location.state} />
-            <ToastContainer />
-        </div>
-    )
+    
+        
+            
+                // if(props.location.state.dish != undefined ){
+                //     return (
+                //         <div>
+                //             Redirecting in 3...2..1
+                //         </div>
+                //     )
+                // } else {
+                    return (
+                        <div className="container mt-4">
+                            <button className="btn btn-dark">
+                                <Link to="/admin" style={{color:'white', textDecoration:'none'}}>Back</Link></button>
+                            <AddDish dish={props.location.state} />
+                            <ToastContainer />
+                        </div>
+                    )
+                // }
+
+            
+
+        
+
+    
 }
 
 export default EditDish;
